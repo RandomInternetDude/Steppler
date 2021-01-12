618ddbb3e347ffe87e7ccb33b0565266d21dc5d6
Joey <joey7040@hotmail.com>
2021-01-12 18:58:53 -0500

    nothing yet really

---
+++ contracts/Election.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.11;
+
+contract Election {
+    \\ required to create contract 
+    \\ store candidate
+    \\ read candidate
+    \\ constructor
+    string public candidate;
+    
+    function Election public {
+        candidate = "Candidate 1";
+    }
+
+
+}
