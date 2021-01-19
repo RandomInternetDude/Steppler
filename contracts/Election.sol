0f3e0b56e6aad79e68a06f016d7b5c2d9e944e21
Joey <joey7040@hotmail.com>
2021-01-19 18:05:32 -0500

    added candidate count incrementer

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,23 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    string public candidate;
-    
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
     function Election () public {
-        candidate = "Candidate 1";
+
+    }
+
+    function addCandidate (stirng _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
 
